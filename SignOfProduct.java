0489e015db7722a77162282bdd878886c5651464
vaibhav <vaibhavmasetty06@gmail.com>
2024-03-12 06:54:26 -0500

    first commit

---
+++ SignOfProduct.java
@@ -0,0 +1,14 @@
+public class SignOfProduct {
+    public int arraySign(int[] nums) {
+        int product = 1;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < 0){
+                product *= -1;
+            }else if(nums[i] == 0){
+                return 0;
+            }
+        }
+        return product;
+    }
+}
